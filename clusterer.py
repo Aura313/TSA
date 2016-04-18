fe2256f00306223b31cca6152b5d86aae381bb01
tanyashah <tanyashah.395@gmail.com>
2016-04-18 23:30:58 +0530

    Random addition to stuff

--- clusterer.py
+++ clusterer.py
@@ -4,18 +4,48 @@ import features
 
 #Some Macros
 NUMBER_OF_CLUSTERS = 3
+
+tweets = [
+			'Tanya has really great boobs',
+			' Priyansh cannot be more dumb',
+			'Tanya REGularly fucks Neelu',
+			'Tanya is short',
+			'So is this sent',
+			'Is this the real life',
+			'is ths fntsy',
+			'cot in da landslyde',
+			'no esc frm reality',
+			'these are some really big ass sentences',
+			'just so that the classifier gets good data',
+			'and can distinguish between small and big sentences'
+		]
+
+kmean = KMeans(n_clusters = NUMBER_OF_CLUSTERS)
+
 def train_kmean():
 
+	global kmean
+
 	feature_list = []
-	for x in range(10):
-		feature_list.append( features.feature_transform("Priyansh is stupid.") )
+	for tweet in tweets:
+		feature_list.append( features.feature_transform(tweet) )
 
 	feature_list = numpy.array(feature_list)
 
 
 	#Stamping out a Kmeans object
-	kmean = KMeans(n_clusters = NUMBER_OF_CLUSTERS)
 	kmean.fit(feature_list)
 
-a = [1,3,6,3,7,4,6,3,7,8]
-print kmean.predict(a)
+train_kmean()
+
+#TESTING AREA
+a = 'short sentence'
+b = 'short SENTENCE '
+c = ' this is a really really really long sentence'
+d = ' THIS is a Long Seentece with CAPS'
+
+
+print kmean.predict(features.feature_transform(a)), a
+print kmean.predict(features.feature_transform(b)), b
+print kmean.predict(features.feature_transform(c)), c
+print kmean.predict(features.feature_transform(d)), d
--- features.py
+++ features.py
@@ -1,13 +1,34 @@
 import random
+import string
 
 
 def _feature_length_of_data_node(data_node):
 	return len(data_node)
 
+def _feature_number_of_cap_letters(data_node):
+	counter = 0
+	for letter in data_node:
+		if letter in string.uppercase:
+			counter += 1
+
+	return counter
+
 def feature_transform(data_node):
-	a = []
-	for x in range(0,10):
-		a.append(random.randint(0,8))
-	return a
+	# a = []
+	# for x in range(0,10):
+	# 	a.append(random.randint(0,8))
+	# return a
+
+	#Declare an empty array of features that we'll be computing for our data node
+	feature_list = []
+
+	#Call the feature that we've created up there
+	feature_length_of_data_node = _feature_length_of_data_node(data_node)
+	feature_number_of_cap_letters = _feature_number_of_cap_letters(data_node)
 
 
+	feature_list.append(feature_length_of_data_node)
+	feature_list.append(feature_number_of_cap_letters)
+
+	#Return this feature list
+	return feature_list
